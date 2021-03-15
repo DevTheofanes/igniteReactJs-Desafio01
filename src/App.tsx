c394404702a6017826826ad464a8f8110a1520f2
DevTheofanes <devtheofanes@gmail.com>
2021-03-15 17:19:24 -0300

    desafio completo

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+
+import { TaskList } from './components/TaskList'
+import { Header } from "./components/Header";
+import './styles/global.scss'
+
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <TaskList />
+    </>
+  )
+}
